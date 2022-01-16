0c6353cd24d43dcdd7ca3f4ed927d8529a5f5199
yassin <hyassin509@gmail.com>
2022-01-16 19:51:39 +0200

    finance logger with tsc

--- tsc-nodejs/config/defaults.ts
+++ tsc-nodejs/config/defaults.ts
@@ -2,6 +2,7 @@
 export const config ={
     port:1337,
     host:"localhost",
+    saltWorkFactor:10,
     dbUri:"mongodb+srv://yassin:KMfZMeHN76fUTCGd@cluster0.8v8fq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 };
 
--- typescript/functions.ts
+++ typescript/functions.ts
@@ -33,3 +33,27 @@ function combine(number1:number | string | boolean,number2:number | string | boo
         console.log(name)
     }
     names("Hagenimana Yassin")
+
+
+    //additional functions
+    type Combinable=number | string;
+type ConversionDescriptor= "as-number" | "as-text";
+
+function combine(n1:Combinable,n2:Combinable){
+  let result;
+  if(typeof n1 === 'number' && n2 === 'number'){
+    result = n1 + n2
+}else{
+   result= n1.toString() +  n2.toString()
+}
+return result;
+}
+const combinedAges=combine(20,30)
+console.log(combinedAges)
+
+const combinedNames=combine("max","Yassin")
+
+console.log(combinedNames)
+
+let names="Hagenimana Yassin";
+console.log("My names are:"+names)
