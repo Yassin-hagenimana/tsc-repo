980c06d1d44f8c488c4911c2a69d35e14d096586
yassin <hyassin509@gmail.com>
2022-01-17 07:53:35 +0200

    dom and type casting

--- typescript/app.ts
+++ typescript/app.ts
@@ -2,3 +2,40 @@ const character ="yassin"
 console.log(character)
 const inputs=document.querySelectorAll("input")
 console.log(inputs)
+
+inputs.forEach(input=>{
+  console.log(input)
+})
+
+//arrays
+let arrayNames=["yassin","hagenimana","Draxler"];
+arrayNames.push("Ki")
+
+//objects
+
+let address={
+  name:"Yassin",
+  district:"Kayonza",
+  age:30,
+  phone:"0786621407"
+}
+
+console.log(address)
+
+const form=document.querySelector("form")!
+//inputs
+const type=document.querySelector("#type") as HTMLSelectElement
+const toFrom=document.querySelector("#tofrom") as HTMLInputElement
+const details=document.querySelector("#details") as HTMLInputElement
+const amount=document.querySelector("#amount") as HTMLInputElement
+
+form.addEventListener("submit",(e:Event)=>{
+  e.preventDefault();
+  
+  console.log(
+    type.value,
+    toFrom.value,
+    details.value,
+    amount.value
+  );
+})
